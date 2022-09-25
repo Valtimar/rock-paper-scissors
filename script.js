7e5fe3627201190593de3cc6dbfb9ad1ae1276fa
Valtimar <lukaszolszowka320@gmail.com>
2022-09-25 20:31:58 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(){
+    return (Math.random() * myArray.length);
+}
+function playRound(playerSelection, computerSelection){
+    
+}
