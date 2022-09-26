b782deb7a7b38d8f738450640870d9ebd96d0363
Valtimar <lukaszolszowka320@gmail.com>
2022-09-26 22:29:20 +0200

    Edit script.js

--- script.js
+++ script.js
@@ -6,8 +6,38 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-
-    
+    console.log(playerSelection)
+    console.log(computerSelection)
+    if (playerSelection == computerSelection){
+        return "It's a draw!";
+    }
+    else if (playerSelection == "rock"){
+        if (computerSelection == "paper"){
+            return "You lose! Paper beats rock";
+        }
+        else{
+            return "You win!";
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "scissors"){
+            return "You lose! Scissors beats Paper";
+        }
+        else{
+            return "You win! Paper beats rock";
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "rock"){
+            return "You lose! Rock beats scissors";
+        }
+        else{
+            return "You win! Scissors beats paper";
+        }
+    }
 }
 
-console.log(getComputerChoice());
+playerSelection = "Rock";
+computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
